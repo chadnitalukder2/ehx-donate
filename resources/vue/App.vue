8999d5e143363bc01b47185ad79fc7f8e45c34d1
chadni54 <chadni@eh.studio>
2025-11-02 23:03:39 +0600

    wip:: navbar

--- resources/vue/App.vue
+++ resources/vue/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <!-- <Navbar /> -->
+    
     <div class="ehxdo_app_content">
+      <Navbar />
+      <div class="ehxdo_app_main_content">
       <div class="ehxdo_sidebar_content">
         <Sidebar />
       </div>
       <div class="ehxdo_main_content">
         <router-view />
+      </div>
       </div>
 
     </div>
@@ -29,14 +32,17 @@ export default {
 
 <style lang="scss" scoped>
 .ehxdo_app_content {
- font-family: Inter Tight, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
+  font-family: Inter Tight, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
   background: #F8F9FC;
   margin-left: -20px;
-  display: flex;
-  gap: 20px;
-  padding: 20px 20px 20px 20px;
 
-  .ehxdo_main_content{
+  .ehxdo_app_main_content {
+    display: flex;
+    gap: 20px;
+    padding: 0px 20px 20px 0px;
+  }
+
+  .ehxdo_main_content {
     flex: 1;
   }
 }
--- resources/vue/components/Layout/Navbar.vue
+++ resources/vue/components/Layout/Navbar.vue
@@ -1,6 +1,34 @@
 <template>
-    <h3>Add New Campaign</h3>
+    <div class="ehxdo_navbar">
+        <div class="ehxdo_navbar_content">
+            <div class="ehxdo_nav_title">
+                <h2>Ehx Donate</h2>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 
 </script>
+<style lang="scss" scoped>
+.ehxdo_navbar {
+    background: #fff;
+    //   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    border: 1px solid #e0e0e0;
+    padding: 20px;
+
+    .ehxdo_navbar_content {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .ehxdo_nav_title {
+            h2 {
+                font-size: 24px;
+                font-weight: bold;
+                margin: 0;
+            }
+        }
+    }
+}
+</style>
