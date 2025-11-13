5274be3cf93996b093cc9cf0c2db34aa1d689d98
chadni54 <chadni@eh.studio>
2025-11-13 18:02:36 +0600

    update style

--- Views/CampaignDetails/CampaignDetails.php
+++ Views/CampaignDetails/CampaignDetails.php
@@ -34,74 +34,76 @@
 
         <div class="ehxdo-left-section">
 
+            <div class="ehxdo_campaign_details_wrapper">
+                <div class="ehxdo-campaign-header-image">
+                    <img src="<?php echo htmlspecialchars($campaign['header_image']); ?>"
+                        alt="<?php echo htmlspecialchars($campaign['title']); ?>"
+                        class="ehxdo-campaign-image">
+                </div>
+                <div class="ehxdo_details_campaign_list">
 
-            <div class="ehxdo-campaign-header-image">
-                <img src="<?php echo htmlspecialchars($campaign['header_image']); ?>"
-                    alt="<?php echo htmlspecialchars($campaign['title']); ?>"
-                    class="ehxdo-campaign-image">
+
+                    <div class="ehxdo_campaign_title_section">
+                        <h1 class="ehxdo-campaign-title"><?php echo htmlspecialchars($campaign['title']); ?></h1>
+                    </div>
+
+                    <div class="ehxdo-stats-container">
+                        <div class="ehxdo-stat-item ehxdo-stat-raised">
+                            <div class="ehxdo-stat-value">$<?php echo number_format($campaign['raised']); ?></div>
+                            <div class="ehxdo-stat-label">Raised</div>
+                        </div>
+                        <div class="ehxdo-stat-item ehxdo-stat-goal">
+                            <div class="ehxdo-stat-value">$<?php echo number_format($campaign['goal_amount']); ?></div>
+                            <div class="ehxdo-stat-label">Goal</div>
+                        </div>
+                        <div class="ehxdo-stat-item ehxdo-stat-donors">
+                            <div class="ehxdo-stat-value"> <?php echo $campaign['donors'] ?? 0; ?></div>
+                            <div class="ehxdo-stat-label">Donors</div>
+                        </div>
+                    </div>
+
+                    <div class="ehxdo-progress-container">
+                        <div class="ehxdo-progress-label">
+                            <span>Campaign Progress</span>
+                            <span class="ehxdo-progress-percent"><?php echo $progress; ?>%</span>
+                        </div>
+                        <div class="ehxdo-progress-bar">
+                            <div class="ehxdo-progress-fill" style="width: <?php echo $progress; ?>%"></div>
+                        </div>
+                    </div>
+
+                    <div class="ehxdo-about-section">
+                        <h2 class="ehxdo-about-title">About This Campaign</h2>
+                        <p class="ehxdo-about-text"><?php echo htmlspecialchars($campaign['short_description']); ?></p>
+                        <p class="ehxdo-about-text"><?php echo htmlspecialchars($campaign['settings']['description']) ?></p>
+                    </div>
+
+                    <div class="ehxdo-campaign-meta">
+                        <span class="ehxdo-meta-item">
+                            <span class="ehxdo-meta-icon">
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5.3335 1.33337V4.00004" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M10.6665 1.33337V4.00004" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M12.6667 2.66663H3.33333C2.59695 2.66663 2 3.26358 2 3.99996V13.3333C2 14.0697 2.59695 14.6666 3.33333 14.6666H12.6667C13.403 14.6666 14 14.0697 14 13.3333V3.99996C14 3.26358 13.403 2.66663 12.6667 2.66663Z" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M2 6.66663H14" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </span> Ends <?php echo htmlspecialchars($campaign['end_date']); ?>
+                        </span>
+                        <span class="ehxdo-meta-item">
+                            <span class="ehxdo-meta-icon">
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M10.6668 14V12.6667C10.6668 11.9594 10.3859 11.2811 9.88578 10.781C9.38568 10.281 8.70741 10 8.00016 10H4.00016C3.29292 10 2.61464 10.281 2.11454 10.781C1.61445 11.2811 1.3335 11.9594 1.3335 12.6667V14" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M10.6665 2.08533C11.2383 2.23357 11.7448 2.5675 12.1063 3.0347C12.4678 3.5019 12.664 4.07592 12.664 4.66666C12.664 5.2574 12.4678 5.83142 12.1063 6.29862C11.7448 6.76582 11.2383 7.09975 10.6665 7.24799" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M14.6665 14V12.6667C14.6661 12.0758 14.4694 11.5019 14.1074 11.0349C13.7454 10.5679 13.2386 10.2344 12.6665 10.0867" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M6.00016 7.33333C7.47292 7.33333 8.66683 6.13943 8.66683 4.66667C8.66683 3.19391 7.47292 2 6.00016 2C4.5274 2 3.3335 3.19391 3.3335 4.66667C3.3335 6.13943 4.5274 7.33333 6.00016 7.33333Z" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                            </span> <?php echo $campaign['donors']; ?> Supporters
+                        </span>
+                    </div>
+                </div>
             </div>
-            <div class="ehxdo_details_campaign_list">
 
-
-                <div class="ehxdo_campaign_title_section">
-                    <h1 class="ehxdo-campaign-title"><?php echo htmlspecialchars($campaign['title']); ?></h1>
-                </div>
-
-                <div class="ehxdo-stats-container">
-                    <div class="ehxdo-stat-item ehxdo-stat-raised">
-                        <div class="ehxdo-stat-value">$<?php echo number_format($campaign['raised']); ?></div>
-                        <div class="ehxdo-stat-label">Raised</div>
-                    </div>
-                    <div class="ehxdo-stat-item ehxdo-stat-goal">
-                        <div class="ehxdo-stat-value">$<?php echo number_format($campaign['goal_amount']); ?></div>
-                        <div class="ehxdo-stat-label">Goal</div>
-                    </div>
-                    <div class="ehxdo-stat-item ehxdo-stat-donors">
-                        <div class="ehxdo-stat-value"><?php echo $campaign['donors']; ?></div>
-                        <div class="ehxdo-stat-label">Donors</div>
-                    </div>
-                </div>
-
-                <div class="ehxdo-progress-container">
-                    <div class="ehxdo-progress-label">
-                        <span>Campaign Progress</span>
-                        <span class="ehxdo-progress-percent"><?php echo $progress; ?>%</span>
-                    </div>
-                    <div class="ehxdo-progress-bar">
-                        <div class="ehxdo-progress-fill" style="width: <?php echo $progress; ?>%"></div>
-                    </div>
-                </div>
-
-                <div class="ehxdo-about-section">
-                    <h2 class="ehxdo-about-title">About This Campaign</h2>
-                    <p class="ehxdo-about-text"><?php echo htmlspecialchars($campaign['short_description']); ?></p>
-                    <p class="ehxdo-about-text"><?php echo htmlspecialchars($campaign['settings']['description']) ?></p>
-                </div>
-
-                <div class="ehxdo-campaign-meta">
-                    <span class="ehxdo-meta-item">
-                        <span class="ehxdo-meta-icon">
-                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M5.3335 1.33337V4.00004" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M10.6665 1.33337V4.00004" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M12.6667 2.66663H3.33333C2.59695 2.66663 2 3.26358 2 3.99996V13.3333C2 14.0697 2.59695 14.6666 3.33333 14.6666H12.6667C13.403 14.6666 14 14.0697 14 13.3333V3.99996C14 3.26358 13.403 2.66663 12.6667 2.66663Z" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M2 6.66663H14" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-                        </span> Ends <?php echo htmlspecialchars($campaign['end_date']); ?>
-                    </span>
-                    <span class="ehxdo-meta-item">
-                        <span class="ehxdo-meta-icon">
-                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M10.6668 14V12.6667C10.6668 11.9594 10.3859 11.2811 9.88578 10.781C9.38568 10.281 8.70741 10 8.00016 10H4.00016C3.29292 10 2.61464 10.281 2.11454 10.781C1.61445 11.2811 1.3335 11.9594 1.3335 12.6667V14" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M10.6665 2.08533C11.2383 2.23357 11.7448 2.5675 12.1063 3.0347C12.4678 3.5019 12.664 4.07592 12.664 4.66666C12.664 5.2574 12.4678 5.83142 12.1063 6.29862C11.7448 6.76582 11.2383 7.09975 10.6665 7.24799" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M14.6665 14V12.6667C14.6661 12.0758 14.4694 11.5019 14.1074 11.0349C13.7454 10.5679 13.2386 10.2344 12.6665 10.0867" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M6.00016 7.33333C7.47292 7.33333 8.66683 6.13943 8.66683 4.66667C8.66683 3.19391 7.47292 2 6.00016 2C4.5274 2 3.3335 3.19391 3.3335 4.66667C3.3335 6.13943 4.5274 7.33333 6.00016 7.33333Z" stroke="#4A5565" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-
-                        </span> <?php echo $campaign['donors']; ?> Supporters
-                    </span>
-                </div>
-            </div>
         </div>
 
         <!-- Right Section -->
@@ -112,18 +114,18 @@
                 <div class="ehxdo-donation-card" id="ehxdo-section-1">
                     <h3 class="ehxdo-card-title">Make a Donation</h3>
 
-                  
+
                     <?php if ($campaign['settings']['allow_recurring_amount'] === true): ?>
-                          <div class="ehxdo-donation-type">
-                        <label for="donation_type" class="ehxdo-label">Donation Type</label>
-                        <select id="donation_type" name="donation_type" class="ehxdo-select-type">
-                            <option value="one-time" selected>One-time</option>
-                            <option value="weekly">Weekly</option>
-                            <option value="monthly">Monthly</option>
-                            <option value="quarterly">Quarterly</option>
-                            <option value="yearly">Yearly</option>
-                        </select>
-                    </div>
+                        <div class="ehxdo-donation-type">
+                            <label for="donation_type" class="ehxdo-label">Donation Type</label>
+                            <select id="donation_type" name="donation_type" class="ehxdo-select-type">
+                                <option value="one-time" selected>One-time</option>
+                                <option value="weekly">Weekly</option>
+                                <option value="monthly">Monthly</option>
+                                <option value="quarterly">Quarterly</option>
+                                <option value="yearly">Yearly</option>
+                            </select>
+                        </div>
 
                         <!-- <div class="ehxdo-donation-type">
                             <label for="donation_type" class="ehxdo-label">Donation Type</label>
@@ -144,9 +146,9 @@
                     <div class="ehxdo-amount-section">
                         <label class="ehxdo-label">Select Amount</label>
                         <input type="hidden" name="amount" id="ehxdo-selected-amount" value="<?php echo $default_amount; ?>">
-                        <?php  if ($campaign['settings']['allow_custom_amount'] === true): ?>
+                        <?php if ($campaign['settings']['allow_custom_amount'] === true): ?>
                             <div class="ehxdo-amount-grid">
-                                <?php  foreach ($campaign['settings']['pricing_items'] as $item): ?>
+                                <?php foreach ($campaign['settings']['pricing_items'] as $item): ?>
                                     <button type="button"
                                         class="ehxdo-amount-btn <?php echo $item['amount'] === $default_amount ? 'ehxdo-selected' : ''; ?>"
                                         data-amount="<?php echo $item['amount']; ?>">
